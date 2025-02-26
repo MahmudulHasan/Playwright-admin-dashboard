29b860ccf7dce4ea57ef090693ffbdaf02135b9f
MahmudulHasan <autul.aust@gmail.com>
2025-02-27 00:01:42 +0600

    Implemented POM

--- pages/dashboard/dashboardPage.ts
+++ pages/dashboard/dashboardPage.ts
@@ -1,26 +1,41 @@
+import { Locator } from "@playwright/test";
+
 export class DashboardPage {
     page: any;
+    url: string;
+    searchBoxLocator: Locator;
+    keyboardPressEnter: string;
+    nextButtonLocator: Locator;
+    prevButtonLocator: Locator;
+    searchProductUrl: string;
+    searchResultLocator: Locator;
     constructor(page) {
         this.page = page;
-
+        this.url = 'https://admin-dashboard-azure-psi.vercel.app/';
+        this.searchBoxLocator = this.page.locator('input[name="q"]');
+        this.keyboardPressEnter = 'Enter';
+        this.nextButtonLocator = this.page.locator('//button[normalize-space()="Next"]');
+        this.prevButtonLocator = this.page.locator('//button[normalize-space()="Prev"]');
+        this.searchProductUrl = 'https://admin-dashboard-azure-psi.vercel.app/?q=Gaming+Laptop+Pro';
+        this.searchResultLocator = this.page.locator('tbody tr td:nth-child(2)');
     }
 
     async gotoDashboardPage(): Promise<void> {
-        await this.page.goto('https://admin-dashboard-azure-psi.vercel.app/');
-        await this.page.locator('input[name="q"]').isVisible();
+        await this.page.goto(this.url);
+        await this.searchBoxLocator.isVisible();
     }
 
     async searchProduct(productName: string): Promise<void> {
-        await this.page.waitForSelector('input[name="q"]');
-        await this.page.fill('input[name="q"]', productName);
-        await this.page.keyboard.press('Enter');
-        await this.page.locator('//button[normalize-space()="Next"]').isDisabled();
-        await this.page.locator('//button[normalize-space()="Prev"]').isEnabled();
-        await this.page.waitForURL('https://admin-dashboard-azure-psi.vercel.app/?q=Gaming+Laptop+Pro');
+        await this.searchBoxLocator.fill(productName);
+        await this.page.keyboard.press(this.keyboardPressEnter);
+        await this.nextButtonLocator.isDisabled();
+        await this.prevButtonLocator.isEnabled();
+        await this.page.waitForURL(this.searchProductUrl);
     }
 
     async getSearchResult(): Promise<string> {
-        return await this.page.locator('tbody tr td:nth-child(2)').first().textContent();
+        console.log(await this.searchResultLocator.textContent());
+        return (await this.searchResultLocator.textContent()) || '';
     }
 }
 module.exports = { DashboardPage };
