58502df3ff250db4c17d75c0178d32422f076600
MahmudulHasan <autul.aust@gmail.com>
2025-02-27 08:55:00 +0600

    Implemented data driven test

--- pages/dashboard/dashboardPage.ts
+++ pages/dashboard/dashboardPage.ts
@@ -1,23 +1,31 @@
-import { Locator } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
 
 export class DashboardPage {
-    page: any;
+    page: Page;
     url: string;
     searchBoxLocator: Locator;
     keyboardPressEnter: string;
     nextButtonLocator: Locator;
     prevButtonLocator: Locator;
     searchProductUrl: string;
-    searchResultLocator: Locator;
-    constructor(page) {
+    productNameLocator: Locator;
+    productStatusLocator: Locator;
+    productPriceLocator: Locator;
+    productSalesLocator: Locator;
+    productCreatedLocator: Locator;
+    constructor(page:Page) {
         this.page = page;
         this.url = 'https://admin-dashboard-azure-psi.vercel.app/';
         this.searchBoxLocator = this.page.locator('input[name="q"]');
         this.keyboardPressEnter = 'Enter';
         this.nextButtonLocator = this.page.locator('//button[normalize-space()="Next"]');
         this.prevButtonLocator = this.page.locator('//button[normalize-space()="Prev"]');
-        this.searchProductUrl = 'https://admin-dashboard-azure-psi.vercel.app/?q=Gaming+Laptop+Pro';
-        this.searchResultLocator = this.page.locator('tbody tr td:nth-child(2)');
+        this.searchProductUrl = 'https://admin-dashboard-azure-psi.vercel.app/?q=';
+        this.productNameLocator = this.page.locator('tbody tr td:nth-child(2)');
+        this.productStatusLocator = this.page.locator('tbody tr td:nth-child(3) div');
+        this.productPriceLocator = this.page.locator('tbody tr td:nth-child(4)');
+        this.productSalesLocator = this.page.locator('tbody tr td:nth-child(5)');
+        this.productCreatedLocator = this.page.locator('tbody tr td:nth-child(6)');
     }
 
     async gotoDashboardPage(): Promise<void> {
@@ -30,12 +38,20 @@ export class DashboardPage {
         await this.page.keyboard.press(this.keyboardPressEnter);
         await this.nextButtonLocator.isDisabled();
         await this.prevButtonLocator.isEnabled();
-        await this.page.waitForURL(this.searchProductUrl);
+        await this.page.waitForURL(this.searchProductUrl+productName.split(' ').join('+'));
     }
 
     async getSearchResult(): Promise<string> {
-        console.log(await this.searchResultLocator.textContent());
-        return (await this.searchResultLocator.textContent()) || '';
+        return (await this.productNameLocator.textContent()) || '';
+    }
+
+    async verifyProductData(product: any): Promise<void> {
+        await this.searchProduct(product.name);
+        expect(await this.productNameLocator.textContent()).toBe(product.name);
+        expect(await this.productStatusLocator.textContent()).toBe(product.status);
+        expect(await this.productPriceLocator.textContent()).toBe("$"+product.price);
+        expect(await this.productSalesLocator.textContent()).toBe(product.sales.toString());
+        expect(await this.productCreatedLocator.textContent()).toBe(product.createdAt);
     }
 }
 module.exports = { DashboardPage };
--- tests/dashboard/dashboard.spec.ts
+++ tests/dashboard/dashboard.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { DashboardPage } from '../../pages/dashboard/dashboardPage';
+import productData from "../../data/products.json";
 
 let dashboardPage: DashboardPage;
 
@@ -9,13 +10,20 @@ test.describe('Dashboard tests', () => {
     dashboardPage = new DashboardPage(page);
   });
 
-  test('Load Admin Dashboard page', async ({ page }) => {
+  test('Load Admin Dashboard page', async () => {
     await dashboardPage.gotoDashboardPage();
   });
 
-  test('Search for a product', async ({ page }) => {
+  test('Search for a product', async () => {
     await dashboardPage.gotoDashboardPage();
-    await dashboardPage.searchProduct('Gaming Laptop Pro');
-    expect(await dashboardPage.getSearchResult()).toBe('Gaming Laptop Pro');
+    await dashboardPage.searchProduct(productData[0].name);
+    expect(await dashboardPage.getSearchResult()).toBe(productData[0].name);
   });
+
+  for(const product of productData) {
+    test(`Verify ${product.name}'s data in the table`, async () => {
+      await dashboardPage.gotoDashboardPage();
+      await dashboardPage.verifyProductData(product);
+    });
+  }
 });
